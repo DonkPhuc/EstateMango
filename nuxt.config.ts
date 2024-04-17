4b83950c9175677268dfa90245d945cfee37de8a
phuc.do <phuc.do@gss-sol.com>
2024-04-17 10:48:38 +0700

    remove pro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: [process.env.NUXT_UI_PRO_PATH || '@nuxt/ui-pro'],
-  modules: [
-    '@nuxt/ui',
-    '@nuxt/fonts',
-    '@vueuse/nuxt'
-  ],
+  modules: ["@nuxt/ui", "@nuxt/fonts", "@vueuse/nuxt"],
   ui: {
-    icons: ['heroicons', 'simple-icons'],
-    safelistColors: ['primary', 'red', 'orange', 'green']
+    icons: ["heroicons", "simple-icons"],
+    safelistColors: ["primary", "red", "orange", "green"],
   },
   devtools: {
-    enabled: true
-  }
-})
+    enabled: true,
+  },
+});
